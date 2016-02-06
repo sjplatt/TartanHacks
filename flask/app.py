44914afd230d2fe4af552f7017465f2058684da3
Sunny Gakhar <sgakhar@andrew.cmu.edu>
2016-02-05 23:55:23 -0500

    sign up and login feature implemented

--- flask/app.py
+++ flask/app.py
@@ -2,8 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for, abort, ses
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'F34TF$($e34D';
+app.config['id'] = 0
 app.config['USERNAME'] = 'test'
 app.config['PASSWORD'] = 'test'
+
+def readFile(path):
+    with open(path, "rt") as f:
+        return f.read()
+
+def writeFile(path, contents):
+    with open(path, "at") as f:
+        f.write(contents)
 
 @app.route('/')
 def home():
@@ -17,26 +26,45 @@ def test():
 def signup():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('test'))
+        session['logged_in'] = True
+        # session['user_logged_in'] = username
+        username = request.form['username']
+        password = request.form['password']
+        writeFile('data/users.txt', '%s\n' % (username))
+        writeFile('data/%s.txt' % (username), 'password:%s' % password)
+        return redirect(url_for('test'))
     return render_template('signup.html')
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
+        username = request.form['username']
+        password = request.form['password']
+        checkFile = False
+        users = readFile('data/users.txt')
+        for user in users.splitlines():
+            print(user, username)
+            if user ==  username:
+                checkFile = True
+                break
+        if checkFile:
+            text = readFile('data/%s.txt' % (username))
+            print(text)
+            print(hash(password))
+            for line in text.splitlines():
+                [key, val] = line.split(":")
+                if key == "password":
+                    passwordHash = (val)
+                    break
+            if passwordHash == (password):
+                # session['logged_in'] = True
+                # session['user_logged_in'] = username
+                return redirect(url_for('test'))
+            else:
+                error = 'Invalid password'
         else:
-            session['logged_in'] = True
-            return redirect(url_for('test'))
+            error = 'Invalid username'
     return render_template('login.html')
 
 
@@ -49,3 +77,54 @@ def login():
 
 if __name__ == '__main__':
     app.run()
+# from flask import Flask, render_template, request, redirect, url_for, abort, session
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'F34TF$($e34D';
+# app.config['USERNAME'] = 'test'
+# app.config['PASSWORD'] = 'test'
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# @app.route('/test', methods = ['GET'])
+# def test():
+#     return render_template('test.html')
+
+# @app.route('/signup', methods = ['GET', 'POST'])
+# def signup():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != app.config['USERNAME']:
+#             error = 'Invalid username'
+#         elif request.form['password'] != app.config['PASSWORD']:
+#             error = 'Invalid password'
+#         else:
+#             session['logged_in'] = True
+#             return redirect(url_for('test'))
+#     return render_template('signup.html')
+
+# @app.route('/login', methods = ['GET','POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != app.config['USERNAME']:
+#             error = 'Invalid username'
+#         elif request.form['password'] != app.config['PASSWORD']:
+#             error = 'Invalid password'
+#         else:
+#             session['logged_in'] = True
+#             return redirect(url_for('test'))
+#     return render_template('login.html')
+
+
+# # @app.route('/message')
+# # def message():
+# #     if not 'username' in session:
+# #         return abort(403)
+# #     return render_template('message.html', username=session['username'], 
+# #                                            message=session['message'])
+
+# if __name__ == '__main__':
+#     app.run()
